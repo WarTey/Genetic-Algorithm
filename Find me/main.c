9db534f79d24794f14e1e7bfc0dc96811dd59e70
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2019-10-08 13:35:51 +0200

    Détermination de la fitness value et affichage de la génération

--- Find me/main.c
+++ Find me/main.c
@@ -14,6 +14,22 @@ typedef struct population {
     int fitness;
 } population;
 
+void fitness(population initPop[10], mot answer, bool found) {
+    for (int i = 0; i < 10; i ++) {
+        for (int j = 0; j < T_MOT; j ++)
+            if (initPop[i].gene[j] == answer[j])
+                initPop[i].fitness += 1;
+        if (initPop[i].fitness == 10)
+            found = true;
+    }
+}
+
+void afficheGeneration(population initPop[10], int generation) {
+    printf("Génération: %d\n", generation);
+    for (int i = 0; i < 10; i ++)
+        printf("Individu n°%d: %s - Fitness: %d\n", i+1, initPop[i].gene, initPop[i].fitness);
+}
+
 int main(void) {
     int generation = 0, indice = 0;
     bool found = false;
@@ -33,4 +49,11 @@ int main(void) {
         initPop[i].gene[T_MOT] = '\0';
         initPop[i].fitness = 0;
     }
+
+    fitness(initPop, answer, found);
+    //while (!found) {
+        afficheGeneration(initPop, generation);
+        generation += 1;
+        
+    //}
 }
