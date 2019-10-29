ddd4c36e7fcfe164e238735a7fda17a59bbd5d49
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2019-10-29 22:25:47 +0100

    Amélioration de l'IA

--- Find me/ga/ga.c
+++ Find me/ga/ga.c
@@ -30,10 +30,10 @@ void afficheGeneration(population *initPop, int generation, int taillePop) {
 // Tirage au sort d'un gène (chaîne) sur une roue biaisée
 void frise(population *initPop, mot gene, int total, int taillePop) {
     // Affectation de la valeur aléatoire
-    int alea = valeurIntervalleZeroUn()*total, old = 0;
+    float alea = valeurIntervalleZeroUn()*total, old = 0;
     for (int j = 0; j < taillePop; j++) {
         // Copie le score de l'individu actuel
-        old += initPop[j].fitness;
+        old += initPop[j].fitness + 10;
         if (alea <= old) {
             // Dans le cas où la valeur aléatoire est inférieure ou égale au score actuel on copie ce gène (chaîne)
             strcpy(gene, initPop[j].gene);
@@ -46,8 +46,8 @@ void frise(population *initPop, mot gene, int total, int taillePop) {
 void paire(population *initPop, population pairePop[][2], int taillePop) {
     int total = 0;
     for (int i = 0; i < taillePop; i++)
-        total += initPop[i].fitness;
-    for (int i = 0; i < taillePop/2; i++) {
+        total += initPop[i].fitness + 10;
+    for (int i = 0; i < taillePop-1; i++) {
         frise(initPop, pairePop[i][0].gene, total, taillePop);
         frise(initPop, pairePop[i][1].gene, total, taillePop);
     }
@@ -56,7 +56,7 @@ void paire(population *initPop, population pairePop[][2], int taillePop) {
 // Définit notre nouvel individu
 void create(mot gene, population pairePop[][2], int indice, int taillePop) {
     // Détermine la valeur aléatoire qui permettra de sélection un certain pourcentage de chaque individu
-    int prop = valeurIntervalleZeroUn()*(taillePop/2);
+    int prop = valeurIntervalleZeroUn()*taillePop;
     // Remplis notre nouvel individu en fonction de la valeur aléatoire précédente
     for (int i = 0; i < prop; i++)
         gene[i] = pairePop[indice][0].gene[i];
@@ -67,29 +67,33 @@ void create(mot gene, population pairePop[][2], int indice, int taillePop) {
 
 // Applique un crossover sur notre population
 void crossover(population *initPop, int taillePop) {
-    population pairePop[taillePop/2][2];
-    population newPop[taillePop/2];
-    // Détermine 5 paires d'individus
+    population pairePop[taillePop-1][2];
+    population newPop[taillePop-1];
+    // Détermine 9 paires d'individus
     paire(initPop, pairePop, taillePop);
-    // Initialise 5 nouveaux individus en fonction des paires précédentes
-    for (int i = 0; i < taillePop/2; i++)
+    // Initialise 9 nouveaux individus en fonction des paires précédentes
+    for (int i = 0; i < taillePop-1; i++)
         create(newPop[i].gene, pairePop, i, taillePop);
+    // Détermine l'individu avec le meilleur score
+    int top = 0;
+    for (int i = 1; i < 10; i++)
+        if (initPop[top].fitness < initPop[i].fitness)
+            top = i;
+    // Remplace les individus par les nouveaux
     int index = 0;
-    // Remplace les individus avec le score le plus faible par les nouveaux individus
-    for (int k = 0; k < taillePop/2; k++) {
-        index = 0;
-        for (int i = 1; i < taillePop; i++)
-            if (initPop[index].fitness > initPop[i].fitness)
-                index = i;
+    for (int k = 0; k < taillePop-1; k++) {
+        if (index == top)
+            index += 1;
         strcpy(initPop[index].gene, newPop[k].gene);
+        index += 1;
     }
 }
 
 // Applique une mutation sur la nouvelle population
 void mutation(population *initPop, chaine dic, int taillePop) {
     for (int i = 0; i < taillePop; i++) {
-        // Une mutation à 10% de chance de s'appliquer sur un des gènes
-        if (valeurIntervalleZeroUn() <= 0.1) {
+        // Une mutation à 20% de chance de s'appliquer sur un des gènes
+        if (valeurIntervalleZeroUn() <= 0.2) {
             // Choisis un gène et un mot du dictionnaire aléatoirement
             int index = valeurIntervalleZeroUn()*10;
             int indice = valeurIntervalleZeroUn()*T_DIC;
